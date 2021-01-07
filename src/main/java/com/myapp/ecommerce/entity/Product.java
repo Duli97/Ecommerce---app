e889b482ba06fe4b6bbe2e2cc0fa5cf8033785f4
Duli97 <dwindula@gmail.com>
2021-01-07 20:52:32 +0530

    no message

---
+++ src/main/java/com/myapp/ecommerce/entity/Product.java
@@ -0,0 +1,49 @@
+package com.myapp.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imgURL;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date createDate;
+
+    @Column(name = "last_updated")
+    private Date lastUpdateDate;
+
+
+}
